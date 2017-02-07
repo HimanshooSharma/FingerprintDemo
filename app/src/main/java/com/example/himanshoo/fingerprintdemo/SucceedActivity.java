7a3bb797bec8b7e45b6f3c190cefc118d1c22804
HimanshooSharma <himanshoosharma@live.com>
2017-02-07 23:25:45 +0530

    SmallLittle Changes

--- app/src/main/java/com/example/himanshoo/fingerprintdemo/SucceedActivity.java
+++ app/src/main/java/com/example/himanshoo/fingerprintdemo/SucceedActivity.java
@@ -1,9 +1,16 @@
 package com.example.himanshoo.fingerprintdemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SucceedActivity extends AppCompatActivity {
+
+    private ImageButton git,about;
+    private String url = "https://github.com/HimanshooSharma/FingerprintDemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,17 @@ public class SucceedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_succeed);
 
         getSupportActionBar().hide();
+
+        git = (ImageButton)findViewById(R.id.git_button);
+        about = (ImageButton)findViewById(R.id.about_button);
+
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
